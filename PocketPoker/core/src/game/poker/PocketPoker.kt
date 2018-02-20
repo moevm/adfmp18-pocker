bdeecf0bf6981a9460b52671045df19e005842a4
Vladimir Turov <aaaaaa2493@yandex.ru>
2018-02-20 15:20:03 +0300

    Partially resolve multiple different screens problem

--- PocketPoker/core/src/game/poker/PocketPoker.kt
+++ PocketPoker/core/src/game/poker/PocketPoker.kt
@@ -1,30 +1,41 @@
 package game.poker
 
-import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 
-class PocketPoker : ApplicationAdapter() {
+class PocketPoker : Game() {
     lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+    lateinit var img: Sprite
+    lateinit var view: Viewport
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        img = Sprite(Texture("badlogic.jpg"))
+        img.setBounds(200f, 200f, 400f, 400f)
+        view = StretchViewport(1080f, 1920f)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.projectionMatrix = view.camera.combined
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        img.draw(batch)
         batch.end()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        view.update(width, height, true)
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        //img.dispose()
     }
 }
