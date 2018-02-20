2ed7a5b0894afa1dc3d211a37bc465b56b255e0c
freepower311 <nikolai.nadezhin@gmail.com>
2018-02-21 00:35:31 +0300

    add logo font style

--- PocketPoker/core/src/game/poker/Fonts.kt
+++ PocketPoker/core/src/game/poker/Fonts.kt
@@ -4,19 +4,27 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter
 
 object Fonts {
     fun generateMenuButtonFont():BitmapFont {
-        val generator = FreeTypeFontGenerator(Gdx.files.internal("arial/arial.ttf"))
+        val generator = FreeTypeFontGenerator(Gdx.files.internal("fonts/arial.ttf"))
         val parameter = FreeTypeFontGenerator.FreeTypeFontParameter()
         parameter.characters = FONT_CHARS
         parameter.size = 50
         parameter.color = Color.BLACK
         return generator.generateFont(parameter)
     }
+    fun generateMenuLogoFont():BitmapFont {
+        val generator = FreeTypeFontGenerator(Gdx.files.internal("fonts/Esteban-Regular.ttf"))
+        val parameter = FreeTypeFontGenerator.FreeTypeFontParameter()
+        parameter.characters = FONT_CHARS
+        parameter.size = 100
+        parameter.color = Color.BLACK
+        return generator.generateFont(parameter)
+    }
     val FONT_CHARS = "абвгдежзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyzАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;:,{}\"´`'<>"
-    val mainMenuButtonFont = generateMenuButtonFont() //generator.generateFont(genFontParam())
+    val mainMenuButtonFont = generateMenuButtonFont()
+    val mainMenuLogoFont = generateMenuLogoFont()
 }
 
 
