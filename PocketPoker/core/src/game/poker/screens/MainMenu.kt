b6e955678831b17a34aa5f5d8eba43847ca062d1
Igor Shadrintcev <ids017@protonmail.com>
2018-02-21 20:04:53 +0300

    main menu textbuttons created

--- PocketPoker/core/src/game/poker/screens/MainMenu.kt
+++ PocketPoker/core/src/game/poker/screens/MainMenu.kt
@@ -1,30 +1,69 @@
 package game.poker.screens
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable
+import com.badlogic.gdx.utils.Align
 
 import game.poker.PocketPoker
-import game.poker.gui.Button
 import game.poker.Settings
 import game.poker.staticFiles.Textures
+import game.poker.staticFiles.Fonts
 
 
 class MainMenu(val game: PocketPoker) : Screen {
-    private val background = Sprite(Textures.mainMenuBg)
-    private val easyGame = Button(200f, 700f, 700f, 150f,
-            Settings.TextKeys.QUICK_GAME)
+
+    private val stage = Stage()
 
     init {
-        background.setBounds(0f, 0f, game.gameWidth, game.gameHeight)
+
+        stage.addActor(Image(Textures.mainMenuBg))
+
+        val buttonSprite = SpriteDrawable(Sprite(Textures.menuButton))
+        val buttonDownSprite = SpriteDrawable(Sprite(Textures.menuButtonDown))
+        val buttonStyle = TextButtonStyle(buttonSprite, buttonDownSprite, buttonSprite,
+                Fonts.mainMenuButtonFont)
+        val labelStyle = LabelStyle(Fonts.mainMenuLogoFont, Color.BLACK)
+
+        val table = Table()
+        table.width = stage.width
+        table.align(Align.center)
+        table.setPosition(0f, Gdx.graphics.height.toFloat() / 2f)
+
+        val titleLabel = Label(Settings.getText(Settings.TextKeys.POKCET_POKER), labelStyle)
+        table.add(titleLabel).padBottom(10f).row()
+
+        val startButton = TextButton(Settings.getText(Settings.TextKeys.QUICK_GAME), buttonStyle)
+        table.add(startButton).padBottom(10f).row()
+
+        val tournamentButton = TextButton(Settings.getText(Settings.TextKeys.TOURNAMENT), buttonStyle)
+        table.add(tournamentButton).padBottom(10f).row()
+
+        val archiveButton = TextButton(Settings.getText(Settings.TextKeys.ARCHIVE), buttonStyle)
+        table.add(archiveButton).padBottom(10f).row()
+
+        val settingsButton = TextButton(Settings.getText(Settings.TextKeys.SETTINGS), buttonStyle)
+        table.add(settingsButton).padBottom(10f).row()
+
+        val exitButton = TextButton(Settings.getText(Settings.TextKeys.EXIT), buttonStyle)
+        table.add(exitButton)
+
+        stage.addActor(table)
+        Gdx.input.inputProcessor = stage
     }
 
     override fun show(){
         println("MainMenu show")
     }
 
-    override fun render(delta: Float){
-        background.draw(game.batch)
-        easyGame.draw(game.batch)
+    override fun render(delta: Float) {
+        stage.draw()
     }
 
     override fun resize(width: Int, height: Int){
--- PocketPoker/core/src/game/poker/staticFiles/Textures.kt
+++ PocketPoker/core/src/game/poker/staticFiles/Textures.kt
@@ -4,5 +4,6 @@ import com.badlogic.gdx.graphics.Texture
 object Textures{
     val mainMenuBg = Texture("pics/MenuBackground.png")
     val menuButton = Texture("pics/button.png")
+    val menuButtonDown = Texture("pics/buttonDown.png")
     val test = Texture("pics/badlogic.jpg")
 }
