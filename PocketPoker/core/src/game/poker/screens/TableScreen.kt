4d275093929f348965f57ba2b998645e77d5544f
freepower311 <nikolai.nadezhin@gmail.com>
2018-03-04 23:40:05 +0300

    fix button position

--- PocketPoker/core/src/game/poker/screens/TableScreen.kt
+++ PocketPoker/core/src/game/poker/screens/TableScreen.kt
@@ -85,28 +85,28 @@ class TableScreen(val game: PocketPoker) : BaseScreen {
     }
 
     private fun setUpCards() {
-        cards.forEach { it.setSize(100f,140f) }
-        cards[0].setPosition(400f,1100f)
-        cards[1].setPosition(510f,1100f)
-        cards[2].setPosition(620f,1100f)
-        cards[3].setPosition(450f,950f)
-        cards[4].setPosition(560f,950f)
+        cards.forEach { it.setSize(100f, 140f) }
+        cards[0].setPosition(400f, 1100f)
+        cards[1].setPosition(510f, 1100f)
+        cards[2].setPosition(620f, 1100f)
+        cards[3].setPosition(450f, 950f)
+        cards[4].setPosition(560f, 950f)
     }
 
     private fun setUpButtons(){
         exitButton.addListener(game.switches[ScreenType.MAIN_MENU])
-        exitButton.setPosition(10f,1790f)
-        exitButton.setSize(120f,120f)
-        chatButton.setPosition(850f,300f)
-        chatButton.setSize(200f,90f)
-        infoButton.setPosition(850f,150f)
-        infoButton.setSize(200f,90f)
-        foldButton.setPosition(20f,20f)
-        foldButton.setSize(200f,90f)
-        callButton.setPosition(240f,20f)
-        callButton.setSize(200f,90f)
-        raiseButton.setPosition(460f,20f)
-        raiseButton.setSize(200f,90f)
+        exitButton.setPosition(10f, 1790f)
+        exitButton.setSize(120f, 120f)
+        chatButton.setPosition(850f, 300f)
+        chatButton.setSize(200f, 90f)
+        infoButton.setPosition(850f, 150f)
+        infoButton.setSize(200f, 90f)
+        foldButton.setPosition(20f, 20f)
+        foldButton.setSize(250f, 90f)
+        callButton.setPosition(370f, 20f)
+        callButton.setSize(350f, 90f)
+        raiseButton.setPosition(800f, 20f)
+        raiseButton.setSize(250f, 90f)
     }
 
     override fun update(){
